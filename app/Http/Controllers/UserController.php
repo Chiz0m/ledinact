b7f94b6f32e3ac71a557a14ddafd76a0841bfea4
Chizom Echehieuka <chizomreal@gmail.com>
2020-11-25 16:54:06 +0100

    testing reset password

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -70,6 +70,7 @@ class UserController extends Controller
         //     'user_type' => 1
         // ]);
         $user = User::where('geo_location', $request->code)->firstOrFail()->update(['password' => bcrypt($request->password)]);
+        $user = User::where('geo_location', $request->code)->firstOrFail()->update(['geo_location' => NULL]);
 
         // $token = $user->createToken('TutsForWeb')->accessToken;
 
